35516d23a9b77fc327638c77d5a6f614c422229c
Matt Conway <mattconway727@gmail.com>
2019-08-07 23:38:51 -0400

    MVP complete

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import OperatorButton from "./OperatorButton";
 
 //Import your array data to from the provided data file
+import { operators } from "../../../data";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [opState, setOpState] = useState(operators);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {opState.map(op => {
+        return <OperatorButton operator={op.char} />;
+      })}
     </div>
   );
 };
+
+export default Operators;
